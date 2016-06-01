58c1a1349e2f9eb35bf41932f304670483090c2f
student <student@PC63207.asost.local>
2016-06-01 12:26:08 +0900

    テストプロジェクト

---
+++ top.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<?php
+    $message = "Hello World!";
+    echo "<h1>$message</h1>";
+?>
+</body>
+</html>
