d10959f8953713286f5bdcea24286db21e75d848
aso1301010 <1301010@st.asojuku.ac.jp>
2016-07-07 12:43:37 +0900

    Update DB.php

--- ACS_System/DB.php
+++ ACS_System/DB.php
@@ -1,7 +1,7 @@
 <?php
 //PDOでDB接続
 
- $pdo = new PDO('mysql:host=52.197.80.228;dbname=acs_system', 'assassin', 'acs6clover');
+ $pdo = new PDO('mysql:host=52.40.177.20;dbname=acs_system', 'assassin', 'acs6clover');
 
 // UTF8
  $pdo->exec("set names utf8");
