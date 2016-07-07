4c8bcb9ca0b37665967d961176ad8c799e5ad9da
MarinaTakatsuka <1301010@st.asojuku.ac.jp>
2016-07-07 22:47:15 +0900

    Update user_name.php

--- ACS_System/user_name.php
+++ ACS_System/user_name.php
@@ -6,11 +6,11 @@ session_start();
 
 $user_id=$_SESSION['user_id'];
 
-//-----------ユーザー名を取り出す処理-----------------
-
+//-----------ユーザー名を取り出す処理---------
 $sql = "SELECT * FROM user WHERE user_id = '$user_id'";
 $data = $pdo->prepare($sql);
 $data->execute();//要らないかも？
+//--------------------------------------------
 
 //---ユーザー名をセッションに格納----------
 while($row = $data ->fetch(PDO::FETCH_ASSOC)){
