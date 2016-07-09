185699043bd80e49fdd3029f376f269692f12134
万理奈 <@marina>
2016-07-09 16:13:16 +0900

    進捗状況を更新するphp追記

---
+++ ACS_System/sintyoku_update/sintyoku_delete.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+require_once 'DB.php';
+
+
+
+$nouhin_delete = $_POST['nouhin_delete'];
+
+$nouhin=$_POST['nouhin_ok'];
+
+
+
+
+require_once 'progress/Progress_situation.php';
+
+
+
+?>
