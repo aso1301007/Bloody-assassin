a68fa5797a47fbdad252dde9bd00ee92479f1c94
万理奈 <@marina>
2016-07-10 15:03:11 +0900

    schoolの進捗管理_ソース更新

--- ACS_System/Shcool/Progress_management/Progress_situation.php
+++ ACS_System/Shcool/Progress_management/Progress_situation.php
@@ -1,28 +1,105 @@
-<!DOCTYPE html>
-<html lang="ja">
-<meta charset="UTF-8">
-<head></head>
-<SCRIPT type="text/javascript"/>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html lang="ja" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
+
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel=stylesheet type=text/css href=css.css>
+
+
+
+<title>進捗状況</title>
+
+
+<script type="text/javascript" src="js/jquery-3.0.0.min.js"></script>
+<script src="js/jquery.focused.min.js"></script>
+
+<script type="text/javascript">
+jQuery(document).ready(function($){
+
+	$("#menu li").hover(function() {
+		$(this).children('ul').show();
+		//window.alert('キャンセルされました');
+	}, function() {
+		$(this).children('ul').hide();
+		//window.alert('キャンセルされました');
+	});
+
+});
+
+
+</script>
+
+
+</head>
+
 <body>
+
 <?php
 
-require_once 'DB.php';
+session_start();
+
+require_once '../../DB.php';
+//require_once 'user_name';
 
 
-//全ページ共通ヘッダーを貼り付け
+/*//-----前ページで指定された注文書のID受け取り-----
+*/
+$_SESSION['sintyoku_tm_id']=1;
+$tm_id=$_SESSION['sintyoku_tm_id'];
+//---------------------------------------
 
 
-//$tyuumon_id=$_SESSION['$tyuumon_id'];
-$tyuumon_id="1";
+//-----------日付と注文書名の取得---------------------
+//$sql1 = "SELECT * FROM tyuumon join hinmei on where tm_id = ? and tyuumon.hin_id = hinmei.hin_id";
+$sql1 = "SELECT * FROM tyuumon where tm_id = ?";
+$data1 = $pdo->prepare($sql1);
+$data1 ->execute(array($tm_id));//要らないかも？
+//print $sql1;
 
-echo $tyuumon_id;
 
 
-//---------進捗を注文書マスター表から取得------------
-$sql = "SELECT * FROM tyuumon_master where tm_id ='.$tyuumon_id'";
+//SELECTでとってきた値を格納
+while($row1 = $data1 -> fetch(PDO::FETCH_ASSOC)){
+//	print "a";
+//	print_r($row1);
+		$t_date = $row1['t_date'];
+		$t_naiyou = $row1['t_naiyou'];
+//		$hin_janru = $row1['hin_janru'];
+		$hin_id=$row1['hin_id'];
+
+
+}
+
+
+//品名取得
+$sql2 = "SELECT * FROM hinmei where hin_id = ?";
+$data2 = $pdo->prepare($sql2);
+$data2 ->execute(array($hin_id));//要らないかも？
+//print $sql1;
+
+
+//SELECTでとってきた値を格納
+while($row2 = $data2 -> fetch(PDO::FETCH_ASSOC)){
+		$hin_janru = $row2['hin_janru'];
+}
+
+
+
+
+
+//------ユーザ名取得---------
+//$user_name=$_SESSION['user_name'];
+$user_name="高塚万理奈";
+//-----------------------------
+
+
+
+ //進捗を注文書マスター表から取得
+$sql = "SELECT * FROM tyuumon_master where tm_id = ?";
 $data = $pdo->prepare($sql);
-$data ->execute();//要らないかも？
+$data ->execute(array($tm_id));//要らないかも？
 
+//SELECTでとってきた値を格納
 while($row = $data -> fetch(PDO::FETCH_ASSOC)){
 		$hattyu = $row['tm_hattyu_flg'];
 		$kakunin = $row['tm_kakunin_flg'];
@@ -32,87 +109,140 @@ while($row = $data -> fetch(PDO::FETCH_ASSOC)){
 		$touroku = $row['tm_touroku_flg'];
 		$houkoku = $row['tm_houkokusho_flg'];
 }
-//-------------------------------------------------------
 
 
 
 
-//--------注文書名を取得------------
-$sql2= "SELECT * FROM tyuumon inner join hinmei on where tyuumon.hin_id=hinmei.hin_id and tm_id='.$tyuumon_id'";
-$data2 = $pdo->prepare($sql2);
-$data2 ->execute();//要らないかも？
+echo <<<EOT
 
-while($row = $data2 -> fetch(PDO::FETCH_ASSOC)){
-	$date = $row['tm_date'];
-	$naiyou=$row['t_naiyou'];
-//	$hin_id=$row['hin_id'];
-	$hin_janru = $row['hin_janru'];
+<div id="header">
+			<div id="top"><a href="../School_Home.php">TOP</a></div>
+			<div id="login_name">$user_name さん</div>
+</div>
+
+<div id="select_menu" style="clear:left;">
+
+	<ul id="menu">
+		<li>ログアウト
+			<ul style="list-style:none;">
+				<li><a href="../../Login/Logout.php">ログアウト</a></li>
+			</ul>
+		</li>
+		<li>注文機能
+			<ul style="list-style:none;">
+				<li><a href="#">新規注文書</a></li>
+				<li><a href="#">注文書選択</a></li>
+			</ul>
+		</li>
+		<li>書類
+			<ul style="list-style:none;">
+				<li><a href="#">書類閲覧</a></li>
+				<li><a href="#">製作物画像登録</a></li>
+			</ul>
+		</li>
+		<li>進捗管理
+			<ul style="list-style:none;">
+				<li><a href="#">進捗管理</a></li>
+			</ul>
+		</li>
+	</ul>
+
+</div>
+<div id="main">
+<div id="border"></div>
+
+<div id="title"><a>進捗状況</a></div>
+
+
+<div id="syorui">
+EOT;
+//注文書名を出力
+echo $t_date."  ・  " .$t_naiyou."  ・  " .$hin_janru;
+
+echo <<<EOT
+</div>
+
+
+<div id="img1">
+EOT;
+//進捗を判定して画像貼り付け
+switch ($hattyu){
+	case 0: echo '<img src="img/hattyuu2.jpg"/>';
+		break;
+	case 1: echo '<img src="img/hattyuu.jpg"/>';
+		break;
 }
-//--------------------------------
 
-
-/*品名取得
-$sql3 = "SELECT * FROM hinmei where hin_id='.$hin_id'";
-$data3 = $pdo->prepare($sql3);
-$data3 ->execute();//要らないかも？
-
-//----繰り返しでSERECTでとってきた値を表示----------
-while($row = $data3 -> fetch(PDO::FETCH_ASSOC)){
-	$hin_janru = $row['hin_janru'];
+switch ($kakunin){
+	case 0: echo '<img src="img/kakunin2.jpg"/>';
+		break;
+	case 1: echo '<img src="img/kakunin.jpg"/>';
+		break;
 }
-*/
 
-//------とってきた日付・注文書名を出力---------------
-echo $date."　・　" .$naiyou."　・　" .$hin_janru;
+switch ($mitumori_now){
+	case 0: echo '<img src="img/mitumori_now2.jpg"/>';
+		break;
+	case 1: echo '<img src="img/mitumori_now.jpg"/>';
+		break;
+}
 
-//--------------------------------------------------
+switch ($mitumori_fin){
+	case 0: echo '<img src="img/mitumori_fin2.jpg"/>';
+		break;
+	case 1: echo '<img src="img/mitumori_fin.jpg"/>';
+		break;
+}
+
+
+echo <<<EOT
+
+</div>
+<div id="img2">
+EOT;
+switch ($nouhin){
+	case 0: echo '<img src="img/nouhin2.jpg"/>';
+		break;
+	case 1: echo '<img src="img/nouhin.jpg"/>';
+		break;
+}
+
+switch ($touroku){
+	case 0: echo '<img src="img/touroku2.jpg"/>';
+		break;
+	case 1: echo '<img src="img/touroku.jpg"/>';
+		break;
+}
+
+switch ($houkoku){
+	case 0: echo '<img src="img/houkoku2.jpg"/>';
+		break;
+	case 1: echo '<img src="img/houkoku.jpg"/>';
+		break;
+}
 ?>
 
-
-<!-------------進捗可視化------------------!>
-
-<!DOCTYPE html>
-<html lang="ja">
-<meta charset="UTF-8">
-<head>
-<style type="text/javascript">
+</div>
 
 
-/*==== 本文のブロック ====*/
-#img{
-width: auto;
-margin: 8px;
-padding: 4px;
-}
-</style>
-</head>
-<body>
 
-<!--
-function showimg1(){
-	document.area1.src = "画像1のURL";
-	document.getElementById("area2").href = "リンク先URL";
-}
 
-function showimg2(){
-	document.area1.src = "画像2のURL";
-	document.getElementById("area2").href = "リンク先URL";
-}
-
-function showimg3(){
-	document.area1.src = "画像3のURL";
-	document.getElementById("area2").href = "リンク先URL";
-}
---!>
+<!-- 進捗状況を変更するボタン -->
+<div id="nouhin_button">
+	<table>
+		<tr><td>
+			<form action="../../sintyoku_update/sintyoku_delete.php" method="post">
+					<input type="submit" value="取消">
+					<input type="hidden" name="flg_name" value="tm_nouhin_flg">
+<!-- 						$_SESSION['flg_name']="nouhin"; -->
+			</form>
+			</td><td>
+			<form action="../../sintyoku_update/sintyoku_ok.php" method="post">
+					<input type="submit" value="OK">
+					<input type="hidden" name="flg_name" value="tm_nouhin_flg">
+			</form>
+			</td></tr>
+	</table>
+</div>
 </body>
-</SCRIPT>
-
-<a href="最初のリンク先URL" id="area2" >
-<img src="最初に表示する画像のURL" name="area1">
-</a>
-
-<input type="button" onclick="showimg1()" value="画像1">
-<input type="button" onclick="showimg2()" value="画像2">
-
 </html>
-
