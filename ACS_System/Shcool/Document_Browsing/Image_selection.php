cf74b5c689e936ed7136f81be4931936e6184e14
万理奈 <@marina>
2016-07-24 17:02:13 +0900

    書類表示

--- ACS_System/Shcool/Document_Browsing/Image_selection.php
+++ ACS_System/Shcool/Document_Browsing/Image_selection.php
@@ -10,8 +10,6 @@
 <title>書類閲覧</title>
 
 
-<script type="text/javascript" src="js/jquery-3.0.0.min.js"></script>
-
 <script type="text/javascript" src="../../js/jquery-3.0.0.min.js"></script>
 <script src="../../js/jquery.focused.min.js"></script>
 <script type="text/javascript">
@@ -31,9 +29,7 @@ jQuery(document).ready(function($){
 </script>
 
 
-<?php require "DB.php";
-//require "../../DB.php";
-?>
+<?php require "../../DB.php";?>
 
 <script type="text/javascript">
 function functionName()
@@ -68,14 +64,14 @@ function functionName()
 	{
 	select2.options[0] = new Option("見積もり","mi");
 	select2.options[1] = new Option("発注","ha");
-
 	}
+
 
 	else if (select1.options[select1.selectedIndex].value == "hin_janru")
 	{
 	<?php while ($pull_h = $pull_hin_janru -> fetch(PDO::FETCH_ASSOC))  { ?>
 
-	select2.options[<?php echo($count_h); ?>] = new Option("<?php echo($pull_h['hin_janru']); ?>","<?php echo($pull_h['t_hin_name']);?>");
+	select2.options[<?php echo($count_h); ?>] = new Option("<?php echo($pull_h['hin_janru']); ?>","<?php echo($pull_h['hin_id']);?>");
 
 	<?php $count_h++; } ?>
 	}
@@ -93,23 +89,23 @@ function functionName()
 
 	</head>
 
+
 	<body onload="functionName()">
 
 	<?php
 
-///session_start();
-//$user_name=$_SESSION['user_name'];   //ユーザー名取得
+session_start();
+$user_name=$_SESSION['user_name'];   //ユーザー名取得
 
 	//ログインしていないorセッションが切れた場合------------
-//	if($user_name==null){
-//		header("Location: ../../Login/login.html");
-//	}
+	if($user_name==null){
+		header("Location: ../../Login/login.html");
+	}
 
 
-$result = $pdo->prepare("SELECT TY.tm_id, TY.t_date, HI.hin_janru, G.gazou_path,tm_seisakubutu FROM (((tyuumon TY
-					INNER JOIN hinmei HI ON TY.t_hin_name = HI.t_hin_name)
-					INNER JOIN tyuumon_master TM ON TY.tm_id=TM.tm_id)
-					LEFT OUTER JOIN gazou G ON TY.tm_id = G.tm_id)ORDER BY TY.t_date ASC LIMIT 9");
+$result = $pdo->prepare("SELECT TY.tm_id, TY.t_date, HI.hin_janru, G.gazou_path FROM ((tyuumon TY
+				INNER JOIN hinmei HI ON TY.t_hin_name = HI.hin_id)
+				LEFT OUTER JOIN gazou G ON TY.tm_id = G.tm_id)ORDER BY TY.t_date ASC LIMIT 9");
 
 $result->execute();
 
@@ -119,17 +115,26 @@ $result->execute();
 
 
 
-	//検索
+	//制作ナンバー検索
 	if(isset($_POST['search_text'])){
 	$search_word = $_POST['search_text'];
 		$search_result= $pdo->prepare("select tyuumon.tm_id,tyuumon.t_date,hinmei.hin_janru,tyuumon_master.tm_seisakubutu,gazou.gazou_path
 				from tyuumon,tyuumon_master,hinmei,gazou
-				where tyuumon.tm_id=tyuumon_master.tm_id and tyuumon.t_hin_name=hinmei.t_hin_name and gazou.tm_id=tyuumon.tm_id and tm_seisakubutu
-				LIKE :keyword");
-		$keyword = "%".$search_word."%";
-		$search_result->bindParam(':keyword', $keyword, PDO::PARAM_STR);
+				where tyuumon.tm_id=tyuumon_master.tm_id and tyuumon.t_hin_name=hinmei.hin_id and gazou.tm_id=tyuumon.tm_id and tm_seisakubutu
+				LIKE '%:keyword%'");
+		$keyword = $search_word;
+		//$search_result->bindParam(":keyword", $keyword, PDO::PARAM_STR);
+		$search_result->bindValue(":keyword", $keyword);
 		$result=$search_result;
 		$result->execute();
+		// 【案件がない場合】
+		$resultSet = $search_result->fetchAll();
+		$resultNum = count($resultSet);
+
+		if (0 == $resultNum) {
+			$non_oblect="検索に一致する案件はありません。";
+		}
+
 	}
 
 
@@ -140,56 +145,92 @@ $result->execute();
 
 		//見積もりor発注
 		if($koumoku=='t_naiyou'){
-			$sort_result= $pdo->prepare("select * from tyuumon,gazou,hinmei
-										where tyuumon.t_naiyou=:zyouken and tyuumon.tm_id=gazou.tm_id and hinmei.t_hin_name=tyuumon.t_hin_name
-										order by :zyouken");
-			$sort_result->bindValue(":zyouken", $zyouken);
+			$sort_result= $pdo->prepare("select * from tyuumon,gazou,hinmei,tyuumon_master
+										where tyuumon.t_naiyou=:zyouken and tyuumon.tm_id=gazou.tm_id and hinmei.hin_id=tyuumon.t_hin_name
+										and tyuumon_master.tm_id=tyuumon.tm_id
+										order by'".":zyouken"."'");
+			$sort_result->bindValue(":zyouken",$zyouken);
 			$result=$sort_result;
 			$result->execute();
+			// 【案件がない場合】
+			$resultSet = $sort_result->fetchAll();
+			$resultNum = count($resultSet);
+
+			if (0 == $resultNum) {
+				$non_oblect="検索に一致する案件はありません。";
+			}
 		}
 
 		//品名ジャンル
 		if($koumoku=='hin_janru'){
-			$sort_result= $pdo->prepare("select * from tyuumon,hinmei,gazou
-										where tyuumon.t_hin_name=hinmei.t_hin_name and tyuumon.tm_id=gazou.tm_id and hinmei.t_hin_name=:zyouken
-										order by :zyouken");
-			$sort_result->bindValue(":zyouken", $zyouken);
+			$sort_result= $pdo->prepare("select * from tyuumon,hinmei,gazou,tyuumon_master
+										where tyuumon.t_hin_name=hinmei.hin_id and tyuumon.tm_id=gazou.tm_id and tyuumon.tm_id=tyuumon_master.tm_id and hinmei.hin_id=:zyouken
+										order by '".":zyouken"."'");
+			$sort_result->bindValue(":zyouken",$zyouken);
 			$result=$sort_result;
 			$result->execute();
+			// 【案件がない場合】
+			$resultSet = $sort_result->fetchAll();
+			$resultNum = count($resultSet);
+
+			if (0 == $resultNum) {
+				$non_oblect="検索に一致する案件はありません。";
+			}
 		}
 
 		//学校名
-
 		if($koumoku=='school_name'){
 			$sort_result= $pdo->prepare("select * from tyuumon,tyuumon_master,tyuumonsha,school,hinmei,gazou
 										where tyuumon.tm_id=tyuumon_master.tm_id and tyuumon_master.user_id=tyuumonsha.user_id and
-										tyuumonsha.school_id=school.school_id and tyuumon.t_hin_name=hinmei.t_hin_name and gazou.tm_id=tyuumon.tm_id and
-										 school.school_id=:zyouken order by :zyouken");
+										tyuumonsha.school_id=school.school_id and tyuumon.t_hin_name=hinmei.hin_id and gazou.tm_id=tyuumon.tm_id and
+										 school.school_id=:zyouken order by '".":zyouken"."'");
 			$sort_result->bindValue(":zyouken", $zyouken);
 			$result=$sort_result;
 			$result->execute();
+			}
+			// 【案件がない場合】
+			$resultSet = $sort_result->fetchAll();
+			$resultNum = count($resultSet);
+
+			if (0 == $resultNum) {
+				$non_oblect="検索に一致する案件はありません。";
 			}
 		}
 
 //日付検索
 		if(isset($_POST['select_year'])){
 			$s_year = $_POST['select_year'];
-			$s_manth = $_POST['select_manth'];
+			$s_month = $_POST['select_month'];
 
 		$date_result= $pdo->prepare("select tyuumon.tm_id,tyuumon.t_date,hinmei.hin_janru,tyuumon_master.tm_seisakubutu,gazou.gazou_path
 									from tyuumon,tyuumon_master,hinmei,gazou
-									where tyuumon.tm_id=tyuumon_master.tm_id and tyuumon.t_hin_name=hinmei.t_hin_name and gazou.tm_id=tyuumon.tm_id
-									and tyuumon.t_date	LIKE :keyword");
-		$keyword = $s_year.'-'.$s_manth."%";//yearとmanthを結合して検索
-		$search_result->bindParam(':keyword', $keyword, PDO::PARAM_STR);
-		$result=$search_result;
+									where tyuumon.tm_id=tyuumon_master.tm_id and tyuumon.t_hin_name=hinmei.hin_id and gazou.tm_id=tyuumon.tm_id
+									and tyuumon.t_date	LIKE ':keyword%'");
+		$keyword = $s_year."-".$s_month;//yearとmonthを結合して検索
+		$date_result->bindValue(":keyword", $keyword);
+		$result=$date_result;
 		$result->execute();
+		// 【案件がない場合】
+		$resultSet = $date_result->fetchAll();
+		$resultNum = count($resultSet);
+
+		if (0 == $resultNum) {
+			$non_oblect="検索に一致する案件はありません。";
 		}
+		}
+
+//日付プルダウンに挿入する西暦の取得
+$date_pull= $pdo->prepare("SELECT DISTINCT SUBSTR(t_date,1,4)AS t_date FROM tyuumon");
+$date_pull->execute();
+if (!$date_pull) {
+	exit('データを登録できませんでした。');
+}
+
 
 	?>
 <div id="header">
 			<input type="button" name="top" value="TOP" onclick="location.href='../School_Home.php'">
-			<div id="login_name"><?php  //echo $user_name;?> さん</div>
+			<div id="login_name"><?php  echo $user_name;?> さん</div>
 
 </div>
 
@@ -229,8 +270,8 @@ $result->execute();
 <!-------製作物ナンバー検索--------------------------->
 <div id="number_search" style="float:left; width:350px; height:70px; border-style: solid; border-width: 1px; margin:20px 5px 5px 20px; padding:10px;" align="left">
 	<b>制作物ナンバーを入力してください。</b>
-	<form  method="post" action="Image_selection.php"style="float:right">
-		<p>制作物ナンバー：<input type="text" style="height:20px; vertical-align: middle;" name="number_search" size="20" maxlength="8" />
+	<form  method="post" action=""style="float:right">
+		<p>制作物ナンバー：<input type="text"value="<?php if(isset($_POST['search_text'])){echo($_POST['search_text']);}?>" style="height:20px; vertical-align: middle;" name="search_text" size="20" maxlength="8" />
 		<span style="margin-right: 1em;"></span>
 		<input type="submit" style="height:32px; vertical-align: middle;" value="表示" /></p>
 	</form>
@@ -242,26 +283,29 @@ $result->execute();
 	<form name="sort_period" method="post"action="">
 		<b>年と月を選択してください。</b>
 		<p>年：
-		<select name = "select_yaer"  >
-			<option value="2016">2016年</option>
-			<option value="2015">2015年</option>
-			<option value="99" selected>選択してください</option>
+		<select name = "select_year" >
+		<?php while ($date_row = $date_pull -> fetch(PDO::FETCH_ASSOC)){ ?>
+
+			<option value="<?php echo($date_row['t_date']);?>"<?php if(isset($_POST['select_month'])){if($_POST['select_year'] == $date_row['t_date']) { print 'selected';}} ?>><?php echo($date_row['t_date']);?></option>
+
+		<?php }?>
+			<option value="99" <?php if(!isset($_POST['select_year'])) { print 'selected';} ?>>選択してください</option>
 		</select><span style="margin-right: 1em;"></span>
 		月：
 		<select name = "select_month" >
-			<option value="01">1月</option>
-			<option value="02">2月</option>
-			<option value="03">3月</option>
-			<option value="04">4月</option>
-			<option value="05">5月</option>
-			<option value="06">6月</option>
-			<option value="07">7月</option>
-			<option value="08">8月</option>
-			<option value="09">9月</option>
-			<option value="10">10月</option>
-			<option value="11">11月</option>
-			<option value="12">12月</option>
-			<option value="99" selected>選択してください</option>
+			<option value="01" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "01") { print 'selected';}} ?>>1月</option>
+			<option value="02" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "02") { print 'selected';}} ?>>2月</option>
+			<option value="03" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "03") { print 'selected';}} ?>>3月</option>
+			<option value="04" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "04") { print 'selected';}} ?>>4月</option>
+			<option value="05" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "05") { print 'selected';}} ?>>5月</option>
+			<option value="06" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "06") { print 'selected';}} ?>>6月</option>
+			<option value="07" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "07") { print 'selected';}} ?>>7月</option>
+			<option value="08" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "08") { print 'selected';}} ?>>8月</option>
+			<option value="09" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "09") { print 'selected';}} ?>>9月</option>
+			<option value="10" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "10") { print 'selected';}} ?>>10月</option>
+			<option value="11" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "11") { print 'selected';}} ?>>11月</option>
+			<option value="12" <?php if(isset($_POST['select_month'])){if($_POST['select_month'] == "12") { print 'selected';}} ?>>12月</option>
+			<option value="99" <?php if(!isset($_POST['select_month'])) { print 'selected';} ?>>選択してください</option>
 		</select><span style="margin-right: 1em;"></span>
 		<input type="submit" style="height:32px; vertical-align: middle;" value="表示" /></p>
 	</form>
@@ -275,10 +319,10 @@ $result->execute();
 		<b>項目と条件を選択してください。</b>
 		<p>項目：
 		<select name = "selectName1" onchange="functionName()">
-			<option value="t_naiyou" label="注文内容" >注文内容</option>
-			<option value="hin_janru" label="品名ジャンル" >品名ジャンル</option>
-			<option value="school_name" label="学校名" >学校名</option>
- 			<option value="99" selected>選択してください</option>
+			<option value="t_naiyou" label="注文内容" 		<?php if(isset($_POST['select_Name1'])){if($_POST['select_Name1'] == "t_naiyou") { print 'selected';}} ?>>注文内容</option>
+			<option value="hin_janru" label="品名ジャンル" 	<?php if(isset($_POST['select_Name1'])){if($_POST['select_Name1'] == "hin_janru") { print 'selected';}} ?>>品名ジャンル</option>
+			<option value="school_name" label="学校名" 		<?php if(isset($_POST['select_Name1'])){if($_POST['select_Name1'] == "school_name") { print 'selected';}} ?>>学校名</option>
+ 			<option value="99" <?php if(!isset($_POST['select_Name1'])) { print 'selected';} ?>>選択してください</option>
 		</select><span style="margin-right: 1em;"></span>
 
 		<a>条件：</a>
@@ -321,7 +365,8 @@ EOT;
 								echo "<br/><br/></div>";
 								$count++;
 		}
-	?>
+		echo "<div style='text-align:center; font-size:1.6em; padding:50px 0px 50px 0px;'>",$non_oblect,"</div>";
+		?>
 
 
 </div>
