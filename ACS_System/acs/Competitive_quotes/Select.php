7123504befa69e10ed2e427f0e252671413a9fec
Hikaru <Hikaru@Hikaru-PC>
2016-08-24 14:40:50 +0900

    ACS側の相みつ機能、制作会社決定機能の白紙を追加

---
+++ ACS_System/acs/Competitive_quotes/Select.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <html lang="ja" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
+
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+<link rel="stylesheet" type="text/css" href="../../css.css"/>
+<link rel="stylesheet" type="text/css" href="css.css"/>
+<title>注文書選択</title>
+
+
+<script type="text/javascript" src="../../js/jquery-3.0.0.min.js"></script>
+<script src="../../js/jquery.focused.min.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function($){
+
+
+	$("#menu li").hover(function() {
+	$(this).children('ul').show();
+}, function() {
+	$(this).children('ul').hide();
+});
+
+
+});
+
+//window.alert('キャンセルされました');
+</script>
+</head>
+<body>
+<?php
+require '../../DB.php';
+include '../acs_header.php';
+?>
+
+
+</body>
+</html>
