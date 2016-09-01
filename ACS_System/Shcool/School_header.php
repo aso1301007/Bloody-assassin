d05f72b4a90c8e6e8ad05a220700f519d6d2bb33
万理奈 <@marina>
2016-09-01 15:12:44 +0900

    School_headerデザイン適応

--- ACS_System/Shcool/School_header.php
+++ ACS_System/Shcool/School_header.php
@@ -12,30 +12,28 @@ $user_name=$_SESSION['user_name'];
 
 ?>
 <div id="header">
-			<input type="button" name="top" value="TOP" onclick="location.href='http://localhost/ACS_System/Shcool/School_Home.php'"style="position: absolute; left: 13em; top: 10px;font-size:16px;"/>
-			<div id="login_name"><?php echo $user_name;?> さん</div>
+			<input type="button" name="top" value="TOP" onclick="window.href='http://localhost/ACS_System/School/School_Home.php'" style="float:left; margin-top:3%; margin-left:5%;">
+				<div id="logo" style="float:left;"><img border="0" src="http://localhost/ACS_System/Login/name2.png" width="300px" alt="ロゴ"></div>
+			<div id="login_name" style="float:left;"><?php echo $user_name;?> さん</div>
 </div>
 
-<div id="select_menu">
-
-	<ul id="menu">
+<div class="menu" style="clear:left;">
+<ul>
 		<li>ログアウト
 			<ul style="list-style:none;">
 				<li><a href="http://localhost/ACS_System/Login/Logout.php" target="_self">ログアウト</a></li>
 			</ul>
 		</li>
-		<li>書類管理
+		<li>注文機能
 			<ul style="list-style:none;">
-				<li><a href="http://localhost/ACS_System/Shcool/New_purchase_order/Entry.php" target="_self">注文書作成</a></li>
+				<li><a href="http://localhost/ACS_System/Shcool/New_purchase_order/Entry.php" target="_self">新規注文書</a></li>
 				<li><a href="http://localhost/ACS_System/Shcool/Purchase_order_selection/Selection.php" target="_self">注文書選択</a></li>
-				<li><a href="http://localhost/ACS_System/Shcool/New_report/Entry.php" target="_self">報告書作成</a></li>
-				<li><a href="#">製作物画像登録</a></li>
 			</ul>
 		</li>
 		<li>書類
 			<ul style="list-style:none;">
 				<li><a href="http://localhost/ACS_System/Shcool/Document_Browsing/Image_selection.php" target="_self">書類閲覧</a></li>
-
+				<li><a href="#">製作物画像登録</a></li>
 			</ul>
 		</li>
 		<li>進捗管理
